41d657bf8c6899833203d3e80e6b26bca4e30d06
Tymek <tymoteusz@akcja.pl>
2021-01-16 20:29:48 +0100

    Changed id properties to UniqueID

--- backend/BiteHACK/WisHUB/models.py
+++ backend/BiteHACK/WisHUB/models.py
@@ -5,14 +5,14 @@ from django.forms import ModelForm
 import datetime
 
 class User(DjangoNode):
-    user_id = IntegerProperty(required=True, unique_index=True)
+    user_id = UniqueIdProperty()
     name = StringProperty(required=True)
     email = StringProperty(required=True)
     posts = RelationshipTo("Post", "POSTED", cardinality=ZeroOrMore)
     comments = RelationshipTo("Comment", "COMMENTED", cardinality=ZeroOrMore)
 
 class Post(DjangoNode):
-    post_id = IntegerProperty(required=True, unique_index=True)
+    post_id = UniqueIdProperty()
     date = DateTimeProperty(default=datetime.datetime.now())
     link = StringProperty(required=True)
     text = StringProperty(required=False)
@@ -21,10 +21,9 @@ class Post(DjangoNode):
     tags = RelationshipTo("Tag", "TAGGED", cardinality=OneOrMore)
     user = RelationshipFrom("User", "POSTED", cardinality=One)
     comments = RelationshipFrom("Comment", "CONCERNING", cardinality=ZeroOrMore)
-    #photo
 
 class Comment(DjangoNode):
-    comment_id = IntegerProperty(required=True, unique_index=True)
+    comment_id = UniqueIdProperty()
     date = DateTimeProperty(default=datetime.datetime.now())
     text = StringProperty(required=True)
     upvoted = IntegerProperty(default=0)
@@ -33,7 +32,7 @@ class Comment(DjangoNode):
     post = RelationshipTo("Post", "CONCERNING", cardinality=One)
 
 class Tag(DjangoNode):
-    tag_id = IntegerProperty(required=True, unique_index=True)
+    tag_id = UniqueIdProperty()
     name = StringProperty(required=True)
     posts = RelationshipFrom("Post", "TAGGED", cardinality=ZeroOrMore)
 
