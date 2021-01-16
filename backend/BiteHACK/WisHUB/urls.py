35cc9f4f7f6f924de02164fb1e08d0d3a8b84817
Tymek <tymoteusz@akcja.pl>
2021-01-17 00:09:01 +0100

    Changed urls.py app name to WisHUB

--- backend/BiteHACK/WisHUB/urls.py
+++ backend/BiteHACK/WisHUB/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from . import views
 from django.conf.urls import url
-app_name = 'chrobry'
+app_name = 'WisHUB'
 urlpatterns = [
     re_path('tags', views.tags),
     re_path('fields', views.fields),
