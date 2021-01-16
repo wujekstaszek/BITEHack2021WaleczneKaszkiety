c5f2506ab0c9bc32c7531506687a1f442146f38d
Tymek <tymoteusz@akcja.pl>
2021-01-16 17:20:01 +0100

    Updated models, added relations

--- backend/BiteHACK/WisHUB/models.py
+++ backend/BiteHACK/WisHUB/models.py
@@ -12,21 +12,29 @@ class User(DjangoNode):
 
 class Post(DjangoNode):
     post_id = IntegerProperty(required=True)
-    link = StringProperty(required=True,)
     date = DateTimeProperty(required=True)
-
+    link = StringProperty(required=True)
+    text = StringProperty(required=False)
+    upvoted = IntegerProperty(default=0)
+    downvoted = IntegerProperty(default=0)
+    tags = RelationshipTo("Tag", "TAGGED", cardinality=OneOrMore)
+    user = RelationshipFrom("User", "POSTED", cardinality=One)
+    comments = RelationshipFrom("Comment", "CONCERNING", cardinality=ZeroOrMore)
     #photo
 
 class Comment(DjangoNode):
+    comment_id = IntegerProperty(required=True)
+    date = DateTimeProperty(required=True)
     text = StringProperty(required=True)
     upvoted = IntegerProperty(default=0)
     downvoted = IntegerProperty(default=0)
+    user = RelationshipFrom("User", "COMMENTED", cardinality=One)
+    post = RelationshipTo("Post", "CONCERNING", cardinality=One)
 
 class Tag(DjangoNode):
     tag_id = IntegerProperty(required=True)
     name = StringProperty(required=True)
-
-
+    posts = RelationshipFrom("Post", "TAGGED", cardinality=ZeroOrMore)
 
 
 
