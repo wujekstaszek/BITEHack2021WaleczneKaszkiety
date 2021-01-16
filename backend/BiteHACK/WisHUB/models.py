1ec829bed04d3eda685d78ff96c58ba6a4fad03e
Tymek <tymoteusz@akcja.pl>
2021-01-16 17:02:16 +0100

    Created first basic models
    No relations yet

--- backend/BiteHACK/WisHUB/models.py
+++ backend/BiteHACK/WisHUB/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from neomodel  import StringProperty,DateTimeProperty,UniqueIdProperty,IntegerProperty,RelationshipTo,RelationshipFrom
+from neomodel.cardinality import *
+from django_neomodel import DjangoNode
+from django.forms import ModelForm
+
+class User(DjangoNode):
+    user_id = IntegerProperty(required=True)
+    name = StringProperty(required=True)
+    email = StringProperty(required=True)
+    posts = RelationshipTo("Post", "POSTED", cardinality=ZeroOrMore)
+    comments = RelationshipTo("Comment", "COMMENTED", cardinality=ZeroOrMore)
+
+class Post(DjangoNode):
+    post_id = IntegerProperty(required=True)
+    link = StringProperty(required=True,)
+    date = DateTimeProperty(required=True)
+
+    #photo
+
+class Comment(DjangoNode):
+    text = StringProperty(required=True)
+    upvoted = IntegerProperty(default=0)
+    downvoted = IntegerProperty(default=0)
+
+class Tag(DjangoNode):
+    tag_id = IntegerProperty(required=True)
+    name = StringProperty(required=True)
+
+
+
+
 
 # Create your models here.
