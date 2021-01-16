875e7e8a1f43992664873a1836b6661b515a2d8f
Proovesor <podbiolm@gmail.com>
2021-01-16 19:33:18 +0100

    further works

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-import './App.scss';
+import "./App.scss";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Switch>
+        <Route></Route>
+      </Switch>
     </div>
   );
 }
