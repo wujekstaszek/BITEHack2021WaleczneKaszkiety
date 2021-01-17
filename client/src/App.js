53a3f51499d3b00eaba3edea915c65d04d447e74
mateuszGorczany <mateusz.g64@interia.pl>
2021-01-17 11:05:13 +0100

    fawikon

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,70 @@
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LinkBox from "./components/Linkbox";
+import SignIn from "./components/Login"
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Topbar from './components/Topbar';
-import Navbar from './components/Navbar';
 import Feed from './pages/Feed';
 import './App.scss';
+
+const items = [
+{
+    title: "Wektory i iteratory",
+    description: "Bardzo fajna strona",
+    link : "https://khanacademy.org",
+    upvotes : 322,
+    downvotes : -22,
+    nComments : 11
+},
+{
+    title : "Wektory i iteratory za darmo",
+    description : "Bardzo fajna strona",
+    link : "https://coursera.com",
+    upvotes : 350,
+    downvotes : -20,
+    nComments : 10
+},
+{
+    title : "Wektory i iteratory udemy przecena z 999999 na 33 zł",
+    description : "Bardzo fajna strona",
+    link : "https://udemy.com",
+    upvotes : 122,
+    downvotes : -31,
+    nComments : 13
+},
+{
+    title : "Wektory yt",
+    description : "Bardzo fajna strona",
+    link : "https://youtube.com",
+    upvotes : 212,
+    downvotes : -10,
+    nComments : 14
+},
+]
 
 const App = ({ routes }) => {
   return (
     <div className="App">
       <Topbar />
       <Navbar />
+      <Switch></Switch>
+          <BrowserRouter>
       <Switch>
+              <Route path="/">
+          {items.map(record => {
+              return <LinkBox {...record}></LinkBox>
+            })}
+                </Route> 
         {routes &&
           routes.map((route) => (
-            <Route exact path={`/${route.toLowerCase()}`} component={Feed} />
-          ))}
+              <Route exact path={`/${route.toLowerCase()}`} component={Feed} />
+              ))}
+         <Route path="/signin">
+            <SignIn/>
+          </Route>
       </Switch>
+              </BrowserRouter>
     </div>
   );
 };
