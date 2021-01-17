d0785e8b8d5e494cfde0adbf15e7e79173ef28b1
wujekstaszek <jaolekm@gmail.com>
2021-01-17 03:14:31 +0100

    prezłożenie backend push

--- backend/BiteHACK/BiteHACK/settings.py
+++ backend/BiteHACK/BiteHACK/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_neomodel',
     'rest_framework',
+    'corsheaders',
     'WisHUB.apps.WishubConfig'
 ]
 
@@ -50,7 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'BiteHACK.urls'
 
@@ -130,3 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
--- backend/BiteHACK/WisHUB/urls.py
+++ backend/BiteHACK/WisHUB/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, re_path
 from . import views
 from django.conf.urls import url
+from rest_framework_simplejwt import views as jwt_views
+
+
 app_name = 'WisHUB'
 urlpatterns = [
     path('tags', views.tags),
     path('fields', views.fields),
-    path('tag/<slug:id>/', views.tag),
-    path('field/<slug:id>/', views.field),
+    path('tag/<slug:tag_id>/', views.tag),
+    path('field/<slug:field_id>/', views.field),
     path('posts/<slug:tag_id>/', views.field),
-    path('post/<slug:id>/', views.field),
+    path('post/<slug:post_id>/', views.field),
+    path('login', views.login),
+    path('register', views.register),
+    path('login_data',views.login_data),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
