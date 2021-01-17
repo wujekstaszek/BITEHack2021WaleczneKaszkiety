9648641ccb80d82883b8c86b89f6f695fcdb6349
mateuszGorczany <mateusz.g64@interia.pl>
2021-01-17 07:51:49 +0100

    cleaning

--- backend/BiteHACK/WisHUB/urls.py
+++ backend/BiteHACK/WisHUB/urls.py
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-# from django.conf.urls import url, path, include
-# from rest_framework import routers
-# from .views import views
-
-from django.urls import path
-from .views import views
-# import views
-# # # from django.urls import include, path
-# # from rest_framework import routers
-# from .views import views
-
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-
-# urlpatterns = [
-    # path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
-# ]
-
-
-urlpatterns = [
-    path('api/lead/', views.PostCreate.as_view() ),
-]
-=======
 from django.urls import path, re_path
 from . import views
 from django.conf.urls import url
@@ -45,4 +18,3 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
->>>>>>> 495e584a9dbcea8e7e4dfd557422ac2e94654621
--- backend/BiteHACK/WisHUB/views.py
+++ backend/BiteHACK/WisHUB/views.py
@@ -1,32 +1,5 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-from .models import Post
-from .serializers import PostSerializer
-from rest_framework import generics
-
-class PostCreate(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('-date_joined')
-#     serializer_class = UserSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-
-
-# class GroupViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows groups to be viewed or edited.
-#     """
-#     queryset = Group.objects.all()
-#     serializer_class = GroupSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-
-
-=======
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -134,4 +107,3 @@ def register(request):
 	return Response(response)
 
 # Create your views here.
->>>>>>> 495e584a9dbcea8e7e4dfd557422ac2e94654621
